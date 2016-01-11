69a29e921f091f0c96d28b3c25d14a9e72566bae
fjonas <fjonas@qq.com>
2016-01-11 19:16:06 +0800

    修改位置判断

--- nail/lib.js
+++ nail/lib.js
@@ -1,15 +1,16 @@
 angular.module('nail', [])
 .directive('nail', function($window) {
-	return function(scope, ele, attr) {
-		ele = ele[0];
-		var _top = ele.getBoundingClientRect().top;
-		ele.style.position = 'relative';
-		var top = attr.nail && Math.min(ele.getBoundingClientRect().top, attr.nail) || ele.getBoundingClientRect().top;
-		$window.onscroll = function() {
-			console.log(_top-$window.scrollY,top)
-			if(_top - $window.scrollY <= top) {
-				ele.style.top = ($window.scrollY + top) + 'px';
-			}
-		}
-	}
+    return function(scope, ele, attr) {
+        ele = ele[0];
+        ele.style.position = 'relative';
+        var top = attr.nail && Math.min(ele.getBoundingClientRect().top, attr.nail) || ele.getBoundingClientRect().top;
+        var _top = ele.getBoundingClientRect().top + top;
+        $window.onscroll = function() {
+            if (_top - $window.scrollY <= top) {
+                ele.style.top = ($window.scrollY + top) + 'px';
+            }else {
+                ele.style.top = _top;
+            }
+        }
+    }
 })
